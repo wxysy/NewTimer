153acbbc446de21c15dceea548a477f36b2321cf
wxysy <wxysy@live.cn>
2025-02-27 19:22:12 +0800

    修复了PPT在取消0时刻事件时，再启动出现两个计时器的现象。本质上不是两个计时器，而是有两个PPT程序。关键代码在PPTCountDown类PPTOpen方法中。

---
+++ NewTimer/ModelDir/MainSettingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace NewTimer.ModelDir
+{
+    record MainSettingModel
+    {
+        public int CountDownSeconds { get; set; } = 12;
+        public Brush CountDownColor { get; set; } = Brushes.Blue;
+        public int WarningSeconds { get; set; } = 6;
+        public Brush WarningColor { get; set; } = Brushes.Red;
+        public int TimerInterval { get; set; } = 1;
+        public bool IsUIControlActived { get; set; } = false;
+        public bool IsZeroEventActived { get; set; } = true;
+    }
+}
